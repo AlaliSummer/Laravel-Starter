99ae050974327ed93e22a88c523dd42ad984fd65
Samar Alali <alalisummer@gmail.com>
2022-07-19 09:17:44 +0300

    Test git pull

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+
+class FirstController extends Controller
+{
+    public function show(){
+        return 'Show String';
+    }
+}
