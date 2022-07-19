5674ca3790706dafe8455bded82d9fc63e750a72
AlaliSummer <57333314+AlaliSummer@users.noreply.github.com>
2022-07-19 10:16:53 +0300

    Update FirstController.php

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class FirstController extends Controller
 {
     public function Show(){
-        return 'String';
+        return 'Summer Hussein Alali';
     }
 }
+
+// Change the string by B2
